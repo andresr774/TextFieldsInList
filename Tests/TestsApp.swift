6651b77a1bc2b7465141d91985829ed59328231c
Andres <andresr774@gmail.com>
2022-03-01 11:05:13 -0500

    Initial Commit

---
+++ Tests/TestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestsApp.swift
+//  Tests
+//
+//  Created by Andres camilo Raigoza misas on 1/03/22.
+//
+
+import SwiftUI
+
+@main
+struct TestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
